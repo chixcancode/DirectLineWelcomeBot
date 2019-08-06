615354fb21a8c908cc820ae8e3b64bba71440486
LaBrina Loving <lalovi@microsoft.com>
2019-08-06 09:27:47 -0400

    Add project files.

---
+++ DirectLineWelcomeBot/Controllers/DirectLineController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectLineWelcomeBot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DirectLineController : ControllerBase
+    {
+        string _directLineUrl = "";
+        string _directLineSecret = "";
+
+
+        public DirectLineController(IConfiguration configuration)
+        {
+            _directLineSecret = configuration["DirectLineSecret"];
+            _directLineUrl = configuration["DirectLineUrl"];
+        }
+        // POST api/<controller>
+        [HttpPost]
+        public async Task<ChatConfig> Token(string value = null)
+        {
+            string token = String.Empty;
+            ChatConfig chatConfig = null;
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(_directLineUrl);
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "v3/directline/tokens/generate");
+            request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", _directLineSecret);
+            var userId = $"dl_{Guid.NewGuid()}";
+            request.Content = new StringContent(
+            JsonConvert.SerializeObject(
+                new { User = new { Id = userId } }),
+                Encoding.UTF8,
+                "application/json");
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var body = await response.Content.ReadAsStringAsync();
+                token = JsonConvert.DeserializeObject<DirectLineToken>(body).token;
+                chatConfig = new ChatConfig()
+                {
+                    Token = token,
+                    UserId = userId
+                };
+            }
+
+
+            return chatConfig;
+        }
+
+
+    }
+}
+public class DirectLineToken
+{
+    public string conversationId { get; set; }
+    public string token { get; set; }
+    public int expires_in { get; set; }
+}
+public class ChatConfig
+{
+    public string Token { get; set; }
+    public string UserId { get; set; }
+}
---
+++ DirectLineWelcomeBot/DirectLineWelcomeBot.cs
@@ -0,0 +1,76 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+
+namespace DirectLineWelcomeBot
+{
+    public class WelcomeUserBot : ActivityHandler
+    {
+        // Messages sent to the user.
+        private const string WelcomeMessage = @"This is a simple Welcome Bot sample. This bot will introduce you
+                                                to welcoming and greeting users. You can say 'intro' to see the
+                                                introduction card. If you are running this bot in the Bot Framework
+                                                Emulator, press the 'Start Over' button to simulate user joining
+                                                a bot or a channel";
+
+        private const string InfoMessage = @"You are seeing this message because the bot received at least one
+                                            'ConversationUpdate' event, indicating you (and possibly others)
+                                            joined the conversation. If you are using the emulator, pressing
+                                            the 'Start Over' button to trigger this event again. The specifics
+                                            of the 'ConversationUpdate' event depends on the channel. You can
+                                            read more information at:
+                                             https://aka.ms/about-botframework-welcome-user";
+
+        private const string PatternMessage = @"It is a good pattern to use this event to send general greeting
+                                              to user, explaining what your bot can do. In this example, the bot
+                                              handles 'hello', 'hi', 'help' and 'intro'. Try it now, type 'hi'";
+
+        private BotState _userState;
+
+        // Initializes a new instance of the "WelcomeUserBot" class.
+        public WelcomeUserBot(UserState userState)
+        {
+            _userState = userState;
+        }
+
+
+        // Greet when users are added to the conversation.
+        // Note that all channels do not send the conversation update activity.
+        // If you find that this bot works in the emulator, but does not in
+        // another channel the reason is most likely that the channel does not
+        // send this activity.
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            await SendIntroCardAsync(turnContext, cancellationToken);
+        }
+
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var text = turnContext.Activity.Text.ToLowerInvariant();
+            await turnContext.SendActivityAsync($"You said {text}.", cancellationToken: cancellationToken);
+
+        }
+
+        private static async Task SendIntroCardAsync(ITurnContext turnContext, CancellationToken cancellationToken)
+        {
+            var card = new HeroCard();
+            card.Title = "Welcome to Bot Framework!";
+            card.Text = @"Welcome to Welcome Users bot sample! This Introduction card is a great way to introduce your Bot to the user and suggest some things to get them started. We use this opportunity to recommend a few next steps for learning more creating and deploying bots.";
+            card.Images = new List<CardImage>() { new CardImage("https://aka.ms/bf-welcome-card-image") };
+            card.Buttons = new List<CardAction>()
+            {
+                new CardAction(ActionTypes.OpenUrl, "Get an overview", null, "Get an overview", "Get an overview", "https://docs.microsoft.com/en-us/azure/bot-service/?view=azure-bot-service-4.0"),
+                new CardAction(ActionTypes.OpenUrl, "Ask a question", null, "Ask a question", "Ask a question", "https://stackoverflow.com/questions/tagged/botframework"),
+                new CardAction(ActionTypes.OpenUrl, "Learn how to deploy", null, "Learn how to deploy", "Learn how to deploy", "https://docs.microsoft.com/en-us/azure/bot-service/bot-builder-howto-deploy-azure?view=azure-bot-service-4.0"),
+            };
+
+            var response = MessageFactory.Attachment(card.ToAttachment());
+            await turnContext.SendActivityAsync(response, cancellationToken);
+        }
+    }
+}
